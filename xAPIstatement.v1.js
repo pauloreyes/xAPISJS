c2358057ee4f8e48c931411ebbabaa8297173a7d
pauloreyes <aijin02@gmail.com>
2022-05-06 00:48:51 +0800

    Update xAPIstatement.v1.js

--- xAPIstatement.v1.js
+++ xAPIstatement.v1.js
@@ -1,7 +1,7 @@
-let sendXAPI = (verbID, verbDisplay, objId, objDisplay) => {
-  const player = GetPlayer();
-  let jsname = player.GetVar('uName');
-  let jsID = player.GetVar('uID');
+let sendXAPI = (verbID, verbDisplay, objId, objDisplay, objDescription, email, uname) => {
+  //const player = GetPlayer();
+  //let jsname = player.GetVar('uName');
+  //let jsemail = player.GetVar('uEmail');
 
   let conf = {
     "endpoint" : "https://xapi-test99.lrs.io/xapi/",
@@ -11,21 +11,31 @@ let sendXAPI = (verbID, verbDisplay, objId, objDisplay) => {
   ADL.XAPIWrapper.changeConfig(conf);
 
   const xAPIstatement = {
-    "actor" : {
-      "name": jsname,
-      "openid": "https://suth.com/employeeID/" + jsID
+
+    "actor": {
+        "mbox": email,
+        "name": uname,
+        "objectType": "Agent"
     },
-    "verb" : {
-      "id": verbID,
-      "display": { "en-us": verbDisplay}
+    "verb": {
+        "id": verbID,
+        "display": {
+            "en-US": verbDisplay
+        }
     },
-    "object" : {
-      "id": objId,
-      "definition": {
-        "name" : { "en-us": objDisplay}
-      }
-    },
-  };
+    "object": {
+        "id": objId,
+        "definition": {
+            "name": {
+                "en-US": objDisplay
+            },
+            "description": {
+                "en-US": objDescription
+            }
+        },
+        "objectType": "Activity"
+    }
+}
     const result = ADL.XAPIWrapper.sendStatement(xAPIstatement);
     console.log('Function executed');
 };
