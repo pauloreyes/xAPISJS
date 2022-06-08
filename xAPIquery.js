4e17ffec35aab14f9ea347121cfc3fd36a7ecd4b
pauloreyes <aijin02@gmail.com>
2022-06-08 20:18:29 +0800

    Update xAPIquery.js

--- xAPIquery.js
+++ xAPIquery.js
@@ -1,14 +1,13 @@
 const conf = {
-    "endpoint" : "https://xapi-test99.lrs.io/xapi/",
-    "auth" : "Basic " + toBase64("tolaha:muzojs")
+    "endpoint": "https://xapi-test99.lrs.io/xapi/",
+    "auth": "Basic " + toBase64("tolaha:muzojs")
   };
-  
-  ADL.XAPIWrapper.changeConfig(conf);
+ADL.XAPIWrapper.changeConfig(conf);
 
-  const params = ADL.XAPIWrapper.searchParams();
+const params = ADL.XAPIWrapper.searchParams();
 
-  params['agent'] = '{"mbox": "mailto:paulo.reyes@gmail.com}';
-  params['verb'] = 'https://clicked.com';
-  params['activity'] = 'https://object.com';
+params['agent'] = '{"mbox": "mailto:paulo.reyes@gmail.com}';
+params['verb'] = 'https://clicked.com';
+params['activity'] = 'https://object.com';
 
-  const xAPIQuery = ADL.XAPIWrapper.getStatements(params);
+const xAPIQuery = ADL.XAPIWrapper.getStatements(params);
